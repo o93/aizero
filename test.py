0c8203df94c56cb0cde150fb4e16101673483046
Hayato Okuda <okuda@a-tm.co.jp>
2018-07-05 12:03:32 +0900

    mnist実装

--- test.py
+++ test.py
@@ -1,12 +1,28 @@
-import numpy as np
+# coding: utf-8
 
+import numpy as np
 import matplotlib
+import mnist
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 1, 0.1)
-y = x
+def step(x):
+  return np.array(x > 0, dtype=np.int)
 
-plt.plot(x,y)
-plt.savefig('plot.png')
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
+
+def relu(x):
+  return np.maximum(0, x)
+
+def identity(x):
+  return x
+
+def softmax(x):
+  max = np.max(x)
+  exp = np.exp(x - max)
+  sum = np.sum(exp)
+  return exp / sum
+
+mnist.load_mnist()
