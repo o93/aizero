f896fc7c28f3d10cb9683e7d699bd8ef77514c28
Hayato Okuda <okuda@a-tm.co.jp>
2018-07-12 12:26:05 +0900

    4章

--- func.py
+++ func.py
@@ -10,6 +10,9 @@ def step(x):
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
+def sigmoid_grad(x):
+  return (1.0 - sigmoid(x)) * sigmoid(x)
+
 def relu(x):
   return np.maximum(0, x)
 
@@ -17,16 +20,61 @@ def identity(x):
   return x
 
 def softmax(x):
-  max = np.max(x)
-  exp = np.exp(x - max)
-  sum = np.sum(exp)
-  return exp / sum
+  if x.ndim == 2:
+    x = x.T
+    x = x - np.max(x, axis=0)
+    y = np.exp(x) / np.sum(np.exp(x), axis=0)
+    return y.T
+
+  x = x - np.max(x)
+  return np.exp(x) / np.sum(np.exp(x))
 
 def mean_squared_error(y, t):
   return 0.5 * np.sum((y - t) ** 2)
 
 def cross_entropy_error(y, t):
-  return -np.sum(t * np.log(y + 1e-7))
+  if y.ndim == 1:
+    t = t.reshape(1, t.size)
+    y = y.reshape(1, y.size)
+
+  if t.size == y.size:
+    t = t.argmax(axis=1)
+
+  batch_size = y.shape[0]
+  return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+def numerical_diff(f, x):
+  h = 1e-4
+  return (f(x + h) - f(x - h)) / (2 * h)
+
+def numerical_gradient(f, x):
+  h = 1e-4
+  grad = np.zeros_like(x)
+
+  it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+  while not it.finished:
+    idx = it.multi_index
+    tmp_val = x[idx]
+    x[idx] = float(tmp_val) + h
+    fxh1 = f(x)
+
+    x[idx] = tmp_val - h
+    fxh2 = f(x)
+    grad[idx] = (fxh1 - fxh2) / (2*h)
+
+    x[idx] = tmp_val
+    it.iternext()
+
+  return grad
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+  x = init_x
+
+  for i in range(step_num):
+    grad = numerical_gradient(f, x)
+    x -= lr * grad
+
+  return x
 
 def save_img(img, file_path):
   pil_img = Image.fromarray(np.uint8(img))
